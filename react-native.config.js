8e8694f0957b90fd48806823b6f9ef62f6629687
ivan <ivan@haxibiao.com>
2021-02-28 13:58:50 +0800

    fix rn config

--- react-native.config.js
+++ react-native.config.js
@@ -6,7 +6,7 @@ const { lstatSync } = require('fs');
  * For faster development workflow, when npm linked
  * we want this library to be transpiled at runtime using the ts-node resolver
  */
-const res = lstatSync(resolve('.', 'node_modules', '@bam.tech', 'react-native-make'));
+const res = lstatSync(resolve('.', 'node_modules', '@haxibiao', 'react-native-make'));
 if (res.isSymbolicLink()) {
   console.warn('Detected linked install of react-native-make, compiling at runtime...');
   require('ts-node').register({ project: resolve(__dirname, `tsconfig.json`) });
